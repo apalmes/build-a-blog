9a5ffed00853e2a75bca0257e808cc3b53d93d3e
asclepiusgal <jeanna.e.clark@gmail.com>
2017-06-28 20:34:07 -0700

    code-review

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Blog App
 ## A simple Flask project for LC-101 students
 
-_Implements_:
+# _Implements_:
 
 - [Flask](http://flask.pocoo.org/) template rendering
 - manual form validation
@@ -11,6 +11,34 @@ _Implements_:
 - request
 - redirect
 - render_template (in place of jinja2)
+- mySQL database
+- SQLAlchemy 
+- cookie sessions
+
+***
+
+# SQLAlchemy setup
+
+open python3 in terminal
+>>> from main import db,Task 
+>>> db
+>>> db.create_all()  ### creates the DB using the specified columns
+>>> new_task = Task('finish Lesson 2')
+>>> db.session.add(new_task) 
+>>> new_task_2 = Task('finish Lesson 3')
+>>> db.session.add(new_task_2) 
+>>> db.session.commit() ### adds the new_task variable to database
+>>> tasks = task.query.all() ### selects everything from the database
+>>> tasks[0].name = 'finish Lesson 2'
+>>> db.session.delete(new_task_2)
+>>> db.drop_all() ### drops all the tables
+>>> db.create_all() ### creates the database with the new / added columns
+
+open mysql in another terminal window 
+> use blog; ## uses the blog database
+> show tables; ## shows the tables
+> describe task; ## shows the table columns
+> select * from task; ## shows the new data inserted into the table columns
 
 ***
 
--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 
 __author__ = "student"
 __version__ = "1.0"
-# June 2017
+# June 26, 2017
 # Flask Blog App re: LaunchCode LC-101
 # Rubric: http://education.launchcode.org/web-fundamentals/assignments/build-a-blog/
 
 
 from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'super_secret_key'
@@ -23,47 +23,56 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    post_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, post_date=None):
         self.title = title
         self.body = body
+        if post_date is None:
+            post_date = datetime.utcnow()
+        self.post_date = post_date
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return redirect('/blog')
+
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blogs():
     blog_entries = Blog.query.all()
     page_title = 'Build-a-Blog'
     main_title = 'Build-a-Blog'
 
     if request.method == 'GET':
-        # requested route format -> ./blog?id=6
-        # refer to Hello Flask apps from curriculum; specifically the hour & minutes /valid-time example
-        # this introduces GET requests with query parameters / data in the URL route
-        # the format is: ```return redirect('/blog?id={0}'.format(id))```
-        id = request.args.get('id')
-        return '<html> blog post </html>...'
+        if 'id' in request.args:
+            blog_id = request.args.get('id')
+            blog_content = Blog.query.get(blog_id)
+            return render_template('post.html', blog_content=blog_content, page_title=page_title)
 
-    return render_template("blog.html", blog_entries=blog_entries, page_title=page_title, main_title=main_title)
+    return render_template('blog.html', main_title=main_title, page_title=page_title, blog_entries=blog_entries)
 
 
-@app.route('/new_post', methods=['POST', 'GET'])
-def new_post():
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
 
         if (title == '') or (body == ''):
-            flash('You left this blank', 'error')
+            flash('Oops, did you forget something...?', 'error')
         else:
             new_blog = Blog(title, body)
             db.session.add(new_blog)
             db.session.commit()
-            return redirect('/')
+            new_blog = Blog.query.order_by('-id').first()
+            new_blog_redirect = new_blog.id
+            return redirect('/blog?id={0}'.format(new_blog_redirect))
 
     page_title = 'New Blog Entry'
     main_title = 'Add New Blog Entry'
 
-    return render_template('new_post.html', page_title=page_title, main_title=main_title)
+    return render_template('newpost.html', page_title=page_title, main_title=main_title)
 
 
 if __name__ == '__main__':
